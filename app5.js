43f371da6e33d052939345e6a0595bcaa1e08d51
rosti <rosti@ROSTISLAV>
2023-04-07 16:53:07 +0200

    Добавьте файлы проекта.

---
+++ app5.js
@@ -0,0 +1,14 @@
+﻿angular.module('myApp', [])
+	.controller('myCtrl', function ($scope) {
+		$scope.users = ['Микола', 'Василь', 'Петро'];
+		$scope.remainingUsers = ['Аня', 'Валя', 'Маша'];
+
+		$scope.addUser = function () {
+			if ($scope.remainingUsers.length > 0) {
+				$scope.users.push($scope.remainingUsers.shift());
+			}
+			if ($scope.remainingUsers.length == 0) {
+				$scope.users.sort();
+			}
+		};
+	});
